0fca1b2f22d8161cdab066d8e01bd358392e3fcb
MMalinowski19 <mikolajmalinowski19@gmail.com>
2023-05-24 19:56:43 +0200

    First commit

---
+++ src/test/java/com/java/pl/isa/homeworks/HomeworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.pl.isa.homeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
